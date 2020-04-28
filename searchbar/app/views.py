74bbf211cc82addb9ea2ddf35cfc2877888adcca
irsa78 <irsamughal0786@gmail.com>
2020-04-28 13:08:39 +0500

    comit

--- searchbar/app/views.py
+++ searchbar/app/views.py
@@ -50,75 +50,75 @@ def is_valid_queryparam(param):
     return param != '' and param is not None
 
 
-def filter(request):
-    qs = Journal.objects.all()
-    categories = Category.objects.all()
-    title_contains_query = request.GET.get('title_contains')
-    id_exact_query = request.GET.get('id_exact')
-    title_or_author_query = request.GET.get('title_or_author')
-    view_count_min = request.GET.get('view_count_min')
-    view_count_max = request.GET.get('view_count_max')
-    date_min = request.GET.get('date_min')
-    date_max = request.GET.get('date_max')
-    category = request.GET.get('category')
-    reviewed = request.GET.get('reviewed')
-    not_reviewed = request.GET.get('notReviewed')
+# def filter(request):
+#     qs = Journal.objects.all()
+#     categories = Category.objects.all()
+#     title_contains_query = request.GET.get('title_contains')
+#     id_exact_query = request.GET.get('id_exact')
+#     title_or_author_query = request.GET.get('title_or_author')
+#     view_count_min = request.GET.get('view_count_min')
+#     view_count_max = request.GET.get('view_count_max')
+#     date_min = request.GET.get('date_min')
+#     date_max = request.GET.get('date_max')
+#     category = request.GET.get('category')
+#     reviewed = request.GET.get('reviewed')
+#     not_reviewed = request.GET.get('notReviewed')
+#
+#     if is_valid_queryparam(title_contains_query):
+#         qs = qs.filter(title__icontains=title_contains_query)
+#
+#     elif is_valid_queryparam(id_exact_query):
+#         qs = qs.filter(id=id_exact_query)
+#
+#     elif is_valid_queryparam(title_or_author_query):
+#         qs = qs.filter(Q(title__icontains=title_or_author_query)
+#                        | Q(author__name__icontains=title_or_author_query)
+#                        ).distinct()
+#
+#     if is_valid_queryparam(view_count_min):
+#         qs = qs.filter(views__gte=view_count_min)
+#
+#     if is_valid_queryparam(view_count_max):
+#         qs = qs.filter(views__lt=view_count_max)
+#
+#     if is_valid_queryparam(date_min):
+#         qs = qs.filter(publish_date__gte=date_min)
+#
+#     if is_valid_queryparam(date_max):
+#         qs = qs.filter(publish_date__lt=date_max)
+#
+#     if is_valid_queryparam(category) and category != 'Choose...':
+#         qs = qs.filter(categories__name=category)
+#
+#     if reviewed == 'on':
+#         qs = qs.filter(reviewed=True)
+#
+#     elif not_reviewed == 'on':
+#         qs = qs.filter(reviewed=False)
+#
+#     return qs
 
-    if is_valid_queryparam(title_contains_query):
-        qs = qs.filter(title__icontains=title_contains_query)
-
-    elif is_valid_queryparam(id_exact_query):
-        qs = qs.filter(id=id_exact_query)
-
-    elif is_valid_queryparam(title_or_author_query):
-        qs = qs.filter(Q(title__icontains=title_or_author_query)
-                       | Q(author__name__icontains=title_or_author_query)
-                       ).distinct()
-
-    if is_valid_queryparam(view_count_min):
-        qs = qs.filter(views__gte=view_count_min)
-
-    if is_valid_queryparam(view_count_max):
-        qs = qs.filter(views__lt=view_count_max)
-
-    if is_valid_queryparam(date_min):
-        qs = qs.filter(publish_date__gte=date_min)
-
-    if is_valid_queryparam(date_max):
-        qs = qs.filter(publish_date__lt=date_max)
-
-    if is_valid_queryparam(category) and category != 'Choose...':
-        qs = qs.filter(categories__name=category)
-
-    if reviewed == 'on':
-        qs = qs.filter(reviewed=True)
-
-    elif not_reviewed == 'on':
-        qs = qs.filter(reviewed=False)
-
-    return qs
-
-
-def infinite_filter(request):
-    limit = request.GET.get('limit')
-    offset = request.GET.get('offset')
-    return Journal.objects.all()[int(offset): int(offset) + int(limit)]
-
-
-def is_there_more_data(request):
-    offset = request.GET.get('offset')
-    if int(offset) > Journal.objects.all().count():
-        return False
-    return True
+#
+# def infinite_filter(request):
+#     limit = request.GET.get('limit')
+#     offset = request.GET.get('offset')
+#     return Journal.objects.all()[int(offset): int(offset) + int(limit)]
 
 
-def BootstrapFilterView(request):
-    qs = filter(request)
-    context = {
-        'queryset': qs,
-        'categories': Category.objects.all()
-    }
-    return render(request, "bootstrap_form.html", context)
+# def is_there_more_data(request):
+#     offset = request.GET.get('offset')
+#     if int(offset) > Journal.objects.all().count():
+#         return False
+#     return True
+
+
+# def BootstrapFilterView(request):
+#     qs = filter(request)
+#     context = {
+#         'queryset': qs,
+#         'categories': Category.objects.all()
+#     }
+#     return render(request, "bootstrap_form.html", context)
 
 
 # class ReactFilterView(generics.ListAPIView):
@@ -148,7 +148,7 @@ def filtr(request):
     qs = Scholarship.objects.all()
     countries = Country.objects.all()
     # name_contains_query=request.GET.get('q')
-    name_contains_query = request.GET.get('title_or_author')
+    name_contains_query = request.GET.get('searchbar')
     # print(name_contains_query)
     # description_exact_query = request.GET.get('id_exact')
     # title_or_author_query = request.GET.get('title_or_author')
@@ -161,7 +161,7 @@ def filtr(request):
     # not_reviewed = request.GET.get('notReviewed')
     # p=request.GET.get('title_contains')
     if is_valid_queryparam(name_contains_query):
-        qs = qs.filtr(Q(name__icontains=name_contains_query) | Q(description__icontains=name_contains_query)).distinct()
+        qs = qs.filter(Q(name__icontains=name_contains_query) | Q(description__icontains=name_contains_query)).distinct()
 
                        # | Q(url__icontains=name_contains_query)
                        # | Q(city__icontains=name_contains_query)
